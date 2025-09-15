a0905881ae814fc4dc4a202debc3f9c123ec69c7
Leonardo Amaro da Silva <93151770+leonardo-amaro@users.noreply.github.com>
2025-09-15 16:46:27 +0000

    finalizando tabuada

--- estudos.rb
+++ estudos.rb
@@ -1,7 +1,7 @@
 puts "=== Tabuada ==="
 
 puts "\nDigite um número para ver a sua tabuada:"
-numero = gets.chomp
+numero = gets.chomp.to_i
 
 1.upto(10) do |valor|
   puts "#{numero} X #{valor} = #{numero * valor}"
