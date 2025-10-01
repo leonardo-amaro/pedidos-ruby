72e6f1a4e923739155737d9147799d186f201164
Leonardo Amaro da Silva <93151770+leonardo-amaro@users.noreply.github.com>
2025-10-01 16:55:59 +0000

    exibindo menu de ações ao usuário

--- pedidos.rb
+++ pedidos.rb
@@ -8,6 +8,17 @@ cardapio = [
 ]
 opcao_usuario = ""
 
+def mostrar_menu
+  puts "PEDIDOS RUBY".center(41, "_")
+  puts "
+  > Bem vindo ao menu de pedidos em Ruby!
+  > Escolha uma das opções abaixo:
+
+  1 - Exibir o cardápio
+  2 - Fazer pedido
+  3 - Sair"
+end
+
 def mostrar_cardapio(c)
   puts "CARDÁPIO".center(31, "_")
   c.each do |pizza|
@@ -15,8 +26,18 @@ def mostrar_cardapio(c)
   end
 end
 
-while opcao_usuario != "n"
-  mostrar_cardapio(cardapio)
-  puts "> Deseja ver novamente? (s/n)"
+while true
+  mostrar_menu
   opcao_usuario = gets.chomp
+
+  case opcao_usuario
+  when "1"
+    mostrar_cardapio(cardapio)
+    puts "> Pressione ENTER para voltar ao menu."
+    gets.chomp
+  when "3"
+    break
+  else
+    puts "> Opção inválida! Voltando para o menu..."
+  end
 end
